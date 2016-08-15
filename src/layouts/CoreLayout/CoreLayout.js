860f876c31e7374fe0a31a55c374b190e6851c63
Levi Voelz <levivoelz@gmail.com>
2016-08-15 16:16:17 -0700

    mobile not supported dialog

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Dialog from 'material-ui/Dialog';
 import classes from './CoreLayout.scss'
 import '../../styles/core.scss'
 
-export const CoreLayout = ({ children }) => (
-  <MuiThemeProvider>
-    <div>
-      <div className={classes.content}>
-        {children}
+export const CoreLayout = ({ children }) => {
+  if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
+    return (
+      <Dialog
+          title="Mobile devices aren't supported yet!"
+          modal={false}
+          open>
+        Sorry! Currently, mobile is not supported. Please try on a desktop device.
+      </Dialog>
+    )
+  }
+
+  return (
+    <MuiThemeProvider>
+      <div>
+        <div className={classes.content}>
+          {children}
+        </div>
       </div>
-    </div>
-  </MuiThemeProvider>
-)
+    </MuiThemeProvider>
+  )
+}
 
 CoreLayout.propTypes = {
   children: React.PropTypes.element.isRequired

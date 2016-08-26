c6b2a24cd6e86dc30e583194712840766150b4fe
Levi Voelz <levivoelz@gmail.com>
2016-08-25 17:10:59 -0700

    add second download button and hide buttons if no images

--- src/routes/Home/containers/ImageDisplayer.js
+++ src/routes/Home/containers/ImageDisplayer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 
 import ImageList from 'components/ImageList'
@@ -8,10 +8,15 @@ const ImageDisplayer = (props) => {
   return (
     <div>
       <h2>Review Images</h2>
-      <DerivativesDownloader />
+      {props.images.length > 0 && <DerivativesDownloader />}
       <ImageList {...props} />
+      {props.images.length > 0 && <DerivativesDownloader />}
     </div>
   )
+}
+
+ImageDisplayer.propTypes = {
+  images: PropTypes.array.isRequired
 }
 
 const mapStateToProps = (state) => ({

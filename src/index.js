32c46a22e6e75f986d842f4cb1750df5bec4838a
Levi Voelz <levivoelz@gmail.com>
2017-12-22 17:44:06 -0800

    reconfig error reporting for react

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
-import store, { history } from './store'
-import App from './App'
+import bugsnag from 'bugsnag-js'
+import createPlugin from 'bugsnag-react'
 import registerServiceWorker from './registerServiceWorker';
 
+import store, { history } from './store'
+import App from './App'
+
+const bugsnagClient = bugsnag('c8e71c39cee3a9318f39dcd5132abe6d')
+const ErrorBoundary = bugsnagClient.use(createPlugin(React))
 const target = document.querySelector('#root')
 
 render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
+  <ErrorBoundary>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+  </ErrorBoundary>,
   target
 )
 

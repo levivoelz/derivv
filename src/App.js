57e95e921f75ae8e3f1b63ff1ac9b1b4c6d66e4b
Levi Voelz <levivoelz@gmail.com>
2017-12-22 17:49:55 -0800

    fix dialog

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import { Home } from 'routes'
+import Dialog, { DialogTitle, DialogContent, DialogContentText } from 'material-ui/Dialog'
 
 class App extends Component {
   render() {
+    if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
+      return (
+        <Dialog
+          title='Mobile device support coming soon!'
+          open>
+          <DialogTitle>Sorry!</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Mobile devices are not yet supported. Please try the app out on your desktop computer.
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
+      )
+    }
+
     return (
-      <div className="App">
+      <div className='App'>
         <main>
-          <Route exact path="/" component={Home} />
+          <Route exact path='/' component={Home} />
         </main>
       </div>
     );
--- src/layouts/CoreLayout.js
+++ src/layouts/CoreLayout.js
@@ -1,20 +1,8 @@
 import React from 'react'
 import { MuiThemeProvider } from 'material-ui/styles'
-import Dialog from 'material-ui/Dialog'
 import theme from 'theme'
 
 export const CoreLayout = ({ children }) => {
-  if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
-    return (
-      <Dialog
-        title='Mobile device support coming soon!'
-        modal={false}
-        open>
-        Sorry! Currently, mobile devices are not yet supported. Please try the app out on your desktop computer.
-      </Dialog>
-    )
-  }
-
   return (
     <MuiThemeProvider theme={theme}>
       {children}

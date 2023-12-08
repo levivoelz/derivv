1dc5af6313866ea2e95ffc0996e7fb89f30bea04
Levi Voelz <levivoelz@gmail.com>
2023-12-07 19:33:12 -0800

    fix: app not building

--- src/index.js
+++ src/index.js
@@ -16,6 +16,12 @@ Bugsnag.start({
 const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React)
 const target = document.querySelector('#root')
 
+class ErrorView extends React.Component {
+  render() {
+    return <div>Sorry! Something went wrong and I'm looking into it. Please refresh the page and try again. If you need support, please send me a message: <a href='https://twitter.com/levivoelz' target='_blank'>@levivoelz</a>.</div>
+  }
+}
+
 render(
   <ErrorBoundary FallbackComponent={ErrorView}>
     <Provider store={store}>
@@ -24,11 +30,5 @@ render(
   </ErrorBoundary>,
   target
 )
-
-class ErrorView extends React.Component {
-  render() {
-    return <div>Sorry! Something went wrong and I'm looking into it. Please refresh the page and try again. If you need support, please send me a message: <a href='https://twitter.com/levivoelz' target='_blank'>@levivoelz</a>.</div>
-  }
-}
 
 unregister()
